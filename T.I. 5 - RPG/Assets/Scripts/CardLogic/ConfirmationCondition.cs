d251757ab72c1d6b95841740bc501ab7c0452cd1
Matucao <lucasmatuck@gmail.com>
2025-08-09 20:32:48 -0300

    Frenzy

--- T.I. 5 - RPG/Assets/Scripts/CardLogic/ConfirmationCondition.cs
+++ T.I. 5 - RPG/Assets/Scripts/CardLogic/ConfirmationCondition.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 [Serializable]
 public abstract class ConfirmationCondition
 {
+    protected enum Target { User, Oponent }
+    protected enum Comparative { Higher, Lower, Iqual, IqualOrHigher, IqualOrLower }
     [NonSerialized] public Effect effect;
     public abstract bool Confirm();
 }
@@ -11,7 +13,6 @@ public abstract class ConfirmationCondition
 public class HasShield : ConfirmationCondition
 {
     [SerializeField] Target CreatureObserved;
-    enum Target { User, Oponent }
     [SerializeField] bool Reverse;
     Creature c;
     public override bool Confirm()
@@ -32,6 +33,39 @@ public class HasShield : ConfirmationCondition
         else
         {
             return c.Shield > 0;
+        }
+    }
+}
+public class HasHealthAmount: ConfirmationCondition
+{
+    [SerializeField] int HealthAmount;
+    [SerializeField] Target CreatureObserved;
+    [SerializeField] Comparative Equation;
+    Creature c;
+    public override bool Confirm()
+    {
+        switch (CreatureObserved)
+        {
+            case Target.Oponent:
+                c = effect.card.deck.Owner.enemy;
+                break;
+            case Target.User:
+                c = effect.card.deck.Owner;
+                break;
+        }
+        switch (Equation)
+        {
+            case Comparative.Higher:
+                return c.Health > HealthAmount;
+            case Comparative.Lower:
+                return c.Health < HealthAmount;
+            case Comparative.Iqual:
+                return c.Health == HealthAmount;
+            case Comparative.IqualOrHigher:
+                return c.Health >= HealthAmount;
+            case Comparative.IqualOrLower:
+                return c.Health <= HealthAmount;
+            default: return false;
         }
     }
 }
