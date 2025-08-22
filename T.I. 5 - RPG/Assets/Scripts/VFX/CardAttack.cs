e4c40adbeac624388dbbf651cedf48ab28bf249f
Perdedo <rafaeltdilascio@gmail.com>
2025-08-21 21:48:58 -0300

    Fixes

--- T.I. 5 - RPG/Assets/Scripts/VFX/CardAttack.cs
+++ T.I. 5 - RPG/Assets/Scripts/VFX/CardAttack.cs
@@ -4,7 +4,7 @@ public class CardAttack : MonoBehaviour
 {
     public Transform target;
     public float moveSpeed, turnSpeed, launchForce;
-    //float launchStart;
+    float launchStart;
     bool moving = false;
     Vector3 aux, startPos, targetPos;
     Rigidbody rb;
@@ -19,30 +19,31 @@ public class CardAttack : MonoBehaviour
     {
         startPos = transform.position;
         targetPos = new Vector3 (target.position.x, startPos.y, target.position.z - target.gameObject.GetComponent<CapsuleCollider>().radius);
-        rb.AddForce(((targetPos - startPos).normalized + Vector3.up) * (targetPos - startPos).magnitude / 2f, ForceMode.Impulse);
-        Invoke("LockIn", 0.75f);
+        //rb.AddForce(((targetPos - startPos).normalized + Vector3.up) * (targetPos - startPos).magnitude / 2f, ForceMode.Impulse);
+        //Invoke("LockIn", 0.75f);
+        LockIn();
     }
 
     public void LockIn()
     {
-        //launchStart = Time.time;
+        launchStart = Time.time;
         moving = true;
     }
 
     void Update()
     {
-        /*if(moving && Time.time - launchStart <= 1)
+        if(moving && Time.time - launchStart <= 1)
         {
             aux = startPos + new Vector3((Time.time - launchStart) * (targetPos.x - startPos.x), ((targetPos - startPos).magnitude/5) * Mathf.Pow((1 - Mathf.Abs(2*(Time.time - launchStart) - 1)), 0.75f), (Time.time - launchStart) * (targetPos.z - startPos.z));
             transform.Translate(aux - transform.position, Space.Self);
-        }*/
-        if (moving)
+        }
+        /*if (moving)
         {
             Vector3 dir = targetPos - transform.position;
             Quaternion targetRotation = Quaternion.LookRotation(dir);
             transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
             transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime, Space.Self);
-        }
+        }*/
     }
 
     private void OnTriggerEnter(Collider other)
